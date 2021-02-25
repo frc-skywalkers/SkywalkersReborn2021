8c6793444b4d81421d062903f436c29fb81b30b9
Chakshan Kothakota <ragchak@gmail.com>
2021-02-24 19:19:06 -0800

    manual way-points and autovoltage constraint added

--- SkwalkersReborn2021/src/main/java/frc/robot/RobotContainer.java
+++ SkwalkersReborn2021/src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -15,8 +16,15 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryParameterizer.TrajectoryGenerationException;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
@@ -39,7 +47,14 @@ public class RobotContainer {
   private final Drivetrain drive = new Drivetrain();
   private final Intake intake = new Intake();
   private final Arm arm = new Arm();
-  private final String [] paths = {"paths/GalacticSearchABlue.wpilib.json", "paths/GalacticSearchARed.wpilib.json", "paths/GalacticSearchBBlue.wpilib.json", "paths/GalacticSearchBRed.wpilib.json", "paths/Slalom.wpilib.json", "paths/slalomV1.wpilib.json"};
+  private final String [] paths = {
+    "paths/GalacticSearchABlue.wpilib.json",
+    "paths/GalacticSearchARed.wpilib.json",
+    "paths/GalacticSearchBBlue.wpilib.json",
+    "paths/GalacticSearchBRed.wpilib.json",
+    "paths/Slalom.wpilib.json",
+    "paths/slalomV1.wpilib.json"
+  };
   private final int pathToRun = 4;
 
   private final SlewRateLimiter speedLimiter = new SlewRateLimiter(3);
@@ -93,17 +108,54 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    String trajectoryJSON = "paths/Slalomv2.wpilib.json";
-    Trajectory trajectory = new Trajectory();
-    try {
-      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-    } catch (IOException ex) {
-      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
-    }
+
+    var autoVoltageConstraint = 
+      new DifferentialDriveVoltageConstraint(
+        new SimpleMotorFeedforward(
+          DriveConstants.ksVolts, 
+          DriveConstants.kvVoltSecondsPerMeter,
+          DriveConstants.kaVoltSecondsSquaredPerMeter), 
+        DriveConstants.kDriveKinematics, 
+        10);
+    
+    TrajectoryConfig config = 
+      new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetersPerSecond, 
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+      .setKinematics(DriveConstants.kDriveKinematics)
+      .addConstraint(autoVoltageConstraint);
+
+    Trajectory slalom = 
+      TrajectoryGenerator.generateTrajectory(
+        new Pose2d(1.23, 0.55, new Rotation2d(0)), 
+        List.of(
+          new Translation2d(3.03, 2.21), 
+          new Translation2d(4.64, 2.75),
+          new Translation2d(6.68, 1.43),
+          new Translation2d(7.34, 0.64),
+          new Translation2d(8.36, 0.75),
+          new Translation2d(8.75, 1.47),
+          new Translation2d(7.83, 2.44),
+          new Translation2d(6.91, 2.01),
+          new Translation2d(6.68, 1.43),
+          new Translation2d(6.38, 0.89),
+          new Translation2d(4.6, 0.48),
+          new Translation2d(2.82, 0.95)), 
+        new Pose2d(1.25, 2.31, new Rotation2d(180)), 
+        config);
+
+
+    // String trajectoryJSON = "paths/Slalomv2.wpilib.json";
+    // Trajectory trajectory = new Trajectory();
+    // try {
+    //   Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+    //   trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    // } catch (IOException ex) {
+    //   DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+    // }
 
     RamseteCommand ramseteCommand = new RamseteCommand(
-        trajectory,
+        slalom,
         drive::getPose,
         new RamseteController(
           AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
@@ -120,7 +172,7 @@ public class RobotContainer {
     );
 
     // Reset odometry to the starting pose of the trajectory.
-    drive.resetOdometry(trajectory.getInitialPose());
+    drive.resetOdometry(slalom.getInitialPose());
 
     // Run path following command, then stop at the end.
     return ramseteCommand.andThen(() -> drive.tankDriveVolts(0, 0));
